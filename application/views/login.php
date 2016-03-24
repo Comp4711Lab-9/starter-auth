6b5c8b61ec8b4b3145b6913138686267d6351086
John Janzen <joalja2002@msn.com>
2016-03-24 16:12:27 -0700

    Up to step 0.5 - new Views and Model

---
+++ application/views/login.php
@@ -0,0 +1,6 @@
+
+<form name="login" method="post" action="/auth/submit">
+    UserID: <input type="text" name="userid"><br/>
+    Password: <input type="password" name="password"><br/>
+    <input type="submit">Submit
+</form>
